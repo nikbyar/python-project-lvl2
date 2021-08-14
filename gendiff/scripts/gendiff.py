4af107430a869284773062f848cb5e8be17c76f1
nikbyar <nikbyar@gmail.com>
2021-08-15 01:40:38 +0300

    step 3

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
+from gendiff.gendiff import generate_diff
 import argparse
-parser = argparse.ArgumentParser(description='Generate diff')
-parser.add_argument("first_file", help="first file for comparison")
-parser.add_argument("second_file", help="second file for comparison")
-parser.add_argument("-f", "--format", default='json', help="set format of output")
-args = parser.parse_args()
+
+
+
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument("first_file", help="first file for comparison", type=str)
+    parser.add_argument("second_file", help="second file for comparison", type=str)
+    parser.add_argument("-f", "--format", default='json', help="set format of output")
+    args = parser.parse_args()
+    generate_diff(args.first_file, args.second_file)
+
+    
+
+if __name__ == '__main__':
+    main()
