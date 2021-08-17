98d5476a812bc9dcbe706ef5162fcef63593e0ae
nikbyar <nikbyar@gmail.com>
2021-08-18 01:50:12 +0300

    added test and fixterus for nested structures

--- gendiff/tests/fixtures/test_string.py
+++ gendiff/tests/fixtures/test_string.py
@@ -4,3 +4,18 @@ STRING = (
     '{0}- proxy: 123.234.53.22 \n{0}- timeout: 50 \n'
     '{0}+ timeout: 20 \n{0}+ verbose: True \n}}'.format(TS, FS)
 )
+
+STRING_NESTED = (
+    '{{\n{1}common: {{\n{0}{1}+ follow: false\n{1}{1}setting1: Value 1\n'
+    '{0}{1}- setting2: 200\n{0}{1}- setting3: true\n{0}{1}+ setting3: null\n'
+    '{0}{1}+ setting4: blah blah\n{0}{1}+ setting5: {{\n'
+    '{1}{1}{1}key5: value5\n{1}{1}}}\n{1}{1}setting6: {{\n{1}{1}{1}doge: {{\n'
+    '{1}{1}{1}{0}- wow:\n{1}{1}{1}{0}+ wow: so much\n{1}{1}{1}}}\n'
+    '{1}{1}{1}key: value\n{1}{1}{0}+ ops: vops\n{1}{1}}}\n{1}}}\n'
+    '{1}group1: {{\n{1}{0}- baz: bas\n{1}{0}+ baz: bars\n{1}{1}foo: bar\n'
+    '{1}{0}- nest: {{\n{1}{1}{1}key: value\n{1}{1}}}\n{1}{0}+ nest: str\n'
+    '{1}}}\n{0}- group2: {{\n{1}{1}abc: 12345\n{1}{1}deep: {{\n'
+    '{1}{1}{1}id: 45\n{1}{1}}}\n{1}}}\n{0}+ group3: {{\n{1}{1}deep: {{\n'
+    '{1}{1}{1}id: {{\n{1}{1}{1}{1}number: 45\n{1}{1}{1}}}\n{1}{1}}}\n'
+    '{1}{1}fee: 100500\n{1}}}\n}}'.format(TS, FS)
+)
--- gendiff/tests/test_gendiff.py
+++ gendiff/tests/test_gendiff.py
@@ -1,19 +1,33 @@
 
 from gendiff.processor import process_to_python_format
 from gendiff.parser import parse_files
-from gendiff.tests.fixtures.test_string import STRING
+from gendiff.tests.fixtures.test_string import STRING, STRING_NESTED
 import pytest
 
 
-TEST1_JSON = 'gendiff/tests/fixtures/test1.json'
-TEST2_JSON = 'gendiff/tests/fixtures/test2.json'
-TEST1_YML = 'gendiff/tests/fixtures/test1.yml'
-TEST2_YML = 'gendiff/tests/fixtures/test2.yml'
+TEST1_JSON1 = 'gendiff/tests/fixtures/test1_1.json'
+TEST1_JSON2 = 'gendiff/tests/fixtures/test1_2.json'
+TEST2_YML1 = 'gendiff/tests/fixtures/test2_1.yml'
+TEST2_YML2 = 'gendiff/tests/fixtures/test2_2.yml'
+TEST3_NESTED_JSON1 = 'gendiff/tests/fixtures/test3_nested1.json'
+TEST3_NESTED_JSON2 = 'gendiff/tests/fixtures/test3_nested2.json'
+TEST4_NESTED_YML1 = 'gendiff/tests/fixtures/test4_nested1.yml'
+TEST4_NESTED_YML1 = 'gendiff/tests/fixtures/test4_nested2.yml'
 
 
 @pytest.mark.parametrize(
-    "input1, input2", [(TEST1_JSON, TEST2_JSON), (TEST1_YML, TEST2_YML)]
+    "input1, input2", [(TEST1_JSON1, TEST1_JSON2), (TEST2_YML1, TEST2_YML2)]
 )
 def test_generate_diff(input1, input2):
     file1, file2 = process_to_python_format(input1, input2)
     assert parse_files(file1, file2) == STRING
+
+
+@pytest.mark.parametrize(
+    "input_nested1, input_nested2", 
+    [(TEST3_NESTED_JSON1, TEST3_NESTED_JSON2),
+    (TEST4_NESTED_YML1, TEST4_NESTED_YML1)]
+)
+def test_generate_diff_nested(input_nested1, input_nested2):
+    file1, file2 = process_to_python_format(input_nested1, input_nested2)
+    assert parse_files(file1, file2) == STRING_NESTED
