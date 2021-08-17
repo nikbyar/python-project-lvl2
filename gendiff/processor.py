5d33985a3ae63e900b3e4717cc1e24e8e9c531f2
nikbyar <nikbyar@gmail.com>
2021-08-18 02:12:16 +0300

    changed proccesor

--- gendiff/processor.py
+++ gendiff/processor.py
@@ -6,10 +6,15 @@ YML = ('.yml', '.yaml')
 
 
 def process_to_python_format(file_path1, file_path2):
-    if file_path1.endswith('.json') and file_path2.endswith('.json'):
-        file1 = json.load(open(file_path1))
-        file2 = json.load(open(file_path2))
-    if file_path1.endswith(YML) and file_path2.endswith(YML):
-        file1 = yaml.full_load(open(file_path1))
-        file2 = yaml.full_load(open(file_path2))
+    for file_path in file_path1, file_path2:
+        if file_path.endswith('.json'):
+            if file_path == file_path1:
+                file1 = json.load(open(file_path))
+            if file_path == file_path2:
+                file2 = json.load(open(file_path))
+        if file_path.endswith(YML):
+            if file_path == file_path1:
+                file1 = yaml.full_load(open(file_path))
+            if file_path == file_path2:
+                file2 = yaml.full_load(open(file_path))
     return file1, file2
--- gendiff/tests/test_gendiff.py
+++ gendiff/tests/test_gendiff.py
@@ -12,7 +12,7 @@ TEST2_YML2 = 'gendiff/tests/fixtures/test2_2.yml'
 TEST3_NESTED_JSON1 = 'gendiff/tests/fixtures/test3_nested1.json'
 TEST3_NESTED_JSON2 = 'gendiff/tests/fixtures/test3_nested2.json'
 TEST4_NESTED_YML1 = 'gendiff/tests/fixtures/test4_nested1.yml'
-TEST4_NESTED_YML1 = 'gendiff/tests/fixtures/test4_nested2.yml'
+TEST4_NESTED_YML2 = 'gendiff/tests/fixtures/test4_nested2.yml'
 
 
 @pytest.mark.parametrize(
