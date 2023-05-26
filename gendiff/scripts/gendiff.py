1f66175a9fd4f041a17714453d4850fb5a4d9ed7
ivan <nikbyar@gmail.com>
2023-05-27 01:45:52 +0400

    step5. add yaml support, add define_format func

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import yaml
 
 
 def get_parser():
@@ -9,18 +10,30 @@ def get_parser():
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-
     first_file = args.first_file
     second_file = args.second_file
     return first_file, second_file
 
 
-def load_json(file1_path, file2_path):
-    return json.load(open(file1_path)), json.load(open(file2_path))
+def load_json(file_path):
+    return json.load(open(file_path))
+
+
+def load_yaml(file_path):
+    return yaml.safe_load(open(file_path))
+
+
+def define_format(file):
+    if file.endswith('.json'):
+        file = load_json(file)
+    elif file.endswith('.yaml') or file.endswith('.yml'):
+        file = load_yaml(file)
+    return file
 
 
 def generate_diff(file1, file2):
-    file1, file2 = load_json(file1, file2)
+    file1 = define_format(file1)
+    file2 = define_format(file2)
     diff = ''
     for key in sorted(file1 | file2):
         if key not in file1:
