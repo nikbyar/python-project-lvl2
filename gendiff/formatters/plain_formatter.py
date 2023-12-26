f5344e7225618e5907de7adfe1706b04771e98aa
ivan <nikbyar@gmail.com>
2023-12-27 01:48:57 +0400

    remove first \n in plain formatter

--- gendiff/formatters/plain_formatter.py
+++ gendiff/formatters/plain_formatter.py
@@ -42,20 +42,8 @@ def build_diff_plain(pre_diff): # noqa: max-complexity=15
 
 def replace_bools_in_plain(diff):
     return diff.replace("False", 'false').replace("None", "null").\
-        replace("True", 'true').rstrip()
+        replace("True", 'true').strip()
+
 
 def make_plain(pre_diff):
     return replace_bools_in_plain(build_diff_plain(pre_diff))
-
-# Property 'common.follow' was added with value: 'False'
-# Property 'common.setting2' was removed
-# Property 'common.setting3' was updated. From True to 'None'
-# Property 'common.setting4' was added with value: 'blah blah'
-# Property 'common.setting5' was added with value: [complex value]
-# Property 'common.setting6.doge.wow' was updated. From '' to 'so much'
-# Property 'common.setting6.ops' was added with value: 'vops'
-# Property 'group1.baz' was updated. From 'bas' to 'bars'
-# Property 'group1.nest' was updated. From [complex value] to 'str'
-# Property 'group2' was removed
-# Property 'group3' was added with value: [complex value]
-# (hexlet-code-py3.9) ivan@MacBook-Air-von-Ivan python-project-lvl2 %
