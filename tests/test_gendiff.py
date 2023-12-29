cc184a58215674cdc035e8303bdfc363eb68003e
ivan <nikbyar@gmail.com>
2023-12-30 03:51:34 +0400

    refactor tests, used pytest.parametrize

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -26,76 +26,32 @@ with open('tests/fixtures/diff_string_plain', 'r') as f:
 diff_string_json = json.dumps(diff_string_stylish)
 
 
-@pytest.fixture
-def file1_json():
-    return 'tests/fixtures/fixture_file1.json'
+file1_json = 'tests/fixtures/fixture_file1.json'
+file2_json = 'tests/fixtures/fixture_file2.json'
+file1_yaml = 'tests/fixtures/fixture_file1.yaml'
+file2_yaml = 'tests/fixtures/fixture_file2.yaml'
+file1_json_nested = 'tests/fixtures/fixture_file1_nested.json'
+file2_json_nested = 'tests/fixtures/fixture_file2_nested.json'
+file1_yaml_nested = 'tests/fixtures/fixture_file1_nested.yaml'
+file2_yaml_nested = 'tests/fixtures/fixture_file2_nested.yaml'
 
 
-@pytest.fixture
-def file2_json():
-    return 'tests/fixtures/fixture_file2.json'
+
+@pytest.mark.parametrize("file1, file2, result", [
+    (file1_json, file2_json, diff_string),
+    (file1_yaml, file2_yaml, diff_string),
+    (file1_json_nested, file2_json_nested, diff_string_stylish),
+    (file1_yaml_nested, file2_yaml_nested, diff_string_stylish)
+])
+def test_generate_diff(file1, file2, result):
+    assert generate_diff(file1, file2) == result
 
 
-def test_generate_diff_json(file1_json, file2_json):
-    result = generate_diff(file1_json, file2_json)
-    assert result == diff_string
+@pytest.mark.parametrize("file1, file2, format, result", [
+    (file1_json_nested, file2_json_nested, 'plain', diff_string_plain),
+    (file1_yaml_nested, file2_yaml_nested, 'plain', diff_string_plain),
+    (file1_json_nested, file2_yaml_nested, 'json', diff_string_json),
+])
+def test_generate_diff_with_format(file1, file2, format, result):
+    assert generate_diff(file1, file2, format) == result
 
-
-@pytest.fixture
-def file1_yaml():
-    return 'tests/fixtures/fixture_file1.yaml'
-
-
-@pytest.fixture
-def file2_yaml():
-    return 'tests/fixtures/fixture_file2.yaml'
-
-
-def test_generate_diff_yaml(file1_yaml, file2_yaml):
-    result = generate_diff(file1_yaml, file2_yaml)
-    assert result == diff_string
-
-
-@pytest.fixture
-def file1_json_nested():
-    return 'tests/fixtures/fixture_file1_nested.json'
-
-
-@pytest.fixture
-def file2_json_nested():
-    return 'tests/fixtures/fixture_file2_nested.json'
-
-
-def test_generate_diff_json_stylish(file1_json_nested, file2_json_nested):
-    result = generate_diff(file1_json_nested, file2_json_nested)
-    assert result == diff_string_stylish
-
-
-@pytest.fixture
-def file1_yaml_nested():
-    return 'tests/fixtures/fixture_file1_nested.yaml'
-
-
-@pytest.fixture
-def file2_yaml_nested():
-    return 'tests/fixtures/fixture_file2_nested.yaml'
-
-
-def test_generate_diff_yaml_stylish(file1_yaml_nested, file2_yaml_nested):
-    result = generate_diff(file1_yaml_nested, file2_yaml_nested)
-    assert result == diff_string_stylish
-
-
-def test_generate_diff_json_plain(file1_json_nested, file2_json_nested):
-    result = generate_diff(file1_json_nested, file2_json_nested, 'plain')
-    assert result == diff_string_plain
-
-
-def test_generate_diff_yaml_plain(file1_yaml_nested, file2_yaml_nested):
-    result = generate_diff(file1_yaml_nested, file2_yaml_nested, 'plain')
-    assert result == diff_string_plain
-
-
-def test_generate_diff_json_output(file1_json_nested, file2_yaml_nested):
-    result = generate_diff(file1_json_nested, file2_yaml_nested, 'json')
-    assert result == diff_string_json
