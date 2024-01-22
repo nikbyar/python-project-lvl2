f9d2b3af26fe74084e3a1d6afa2967c255cb6a81
ivan <nikbyar@gmail.com>
2024-01-22 04:26:12 +0400

    mentors notes N8,9

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,9 +2,13 @@ from gendiff.parser import get_parser
 from gendiff.format_selector import select_format
 
 
-def main():
+def generate_diff():
     first_file, second_file, file_format = get_parser()
-    print(select_format(first_file, second_file, file_format))
+    return select_format(first_file, second_file, file_format)
+
+
+def main():
+    print(generate_diff())
 
 
 if __name__ == '__main__':
