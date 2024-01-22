282388fc20a3ade5ad9e7655d6ae85eb7f7b326a
ivan <nikbyar@gmail.com>
2024-01-22 04:33:47 +0400

    edit gendiff

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -2,13 +2,15 @@ from gendiff.parser import get_parser
 from gendiff.format_selector import select_format
 
 
-def generate_diff():
-    first_file, second_file, file_format = get_parser()
+first_file, second_file, file_format = get_parser()
+
+
+def generate_diff(first_file, second_file, file_format):
     return select_format(first_file, second_file, file_format)
 
 
 def main():
-    print(generate_diff())
+    print(generate_diff(first_file, second_file, file_format))
 
 
 if __name__ == '__main__':
